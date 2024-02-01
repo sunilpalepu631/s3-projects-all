086f20837ea6cd998022a075ab6a7e264689605d
sunilpalepu631 <sunil.p@orotron.com>
2024-02-01 16:40:02 +0530

    Initial commit

---
+++ s3-uploads-main/uploadsapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class ModelUploads(models.Model):
+    object_key= models.CharField(max_length=200)
+    file_name = models.CharField(max_length=200)
+    file_size = models.CharField(max_length=200,default='something')
+   
+
+    
+
